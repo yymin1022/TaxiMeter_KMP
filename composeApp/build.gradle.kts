1400cb54e8c016efe98bf888b6b97c9c90e74ea3
yymin1022 <yymin1022@gmail.com>
2025-10-02 16:44:38 +0900

    Update Project : Added Koin dependency

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,4 +1,3 @@
-import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
@@ -39,6 +38,11 @@ kotlin {
             implementation(compose.components.uiToolingPreview)
             implementation(libs.androidx.lifecycle.viewmodelCompose)
             implementation(libs.androidx.lifecycle.runtimeCompose)
+
+            // Koin Dependency
+            val koinVersion = "4.1.1"
+            implementation("io.insert-koin:koin-core:$koinVersion")
+            implementation("io.insert-koin:koin-compose:$koinVersion")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
