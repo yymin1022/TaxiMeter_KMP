6354b8013becf3450cf5d20d8321133e46fe1de1
yymin1022 <yymin1022@gmail.com>
2025-10-03 15:14:53 +0900

    Update App Main : Applied Safe Area

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/App.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/App.kt
@@ -1,7 +1,13 @@
 package com.yong.taximeter
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.safeDrawing
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.navigator.Navigator
 import com.yong.taximeter.ui.main.MainScreen
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -10,7 +16,14 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
-        // Main UI
-        Navigator(MainScreen)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .windowInsetsPadding(WindowInsets.safeDrawing)
+        ) {
+            // Main UI
+            Navigator(MainScreen)
+        }
+
     }
 }
