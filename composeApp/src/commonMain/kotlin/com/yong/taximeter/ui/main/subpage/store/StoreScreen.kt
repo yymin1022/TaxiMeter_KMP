d5f7fb8410dcf63fa44a2ecc0abc738f692d9fec
yymin1022 <yymin1022@gmail.com>
2025-10-03 15:32:50 +0900

    Added Store Screen / ViewModel

---
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subpage/store/StoreScreen.kt
@@ -0,0 +1,21 @@
+package com.yong.taximeter.ui.main.subpage.store
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import cafe.adriel.voyager.core.screen.Screen
+
+object StoreScreen: Screen {
+    @Composable
+    override fun Content() {
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            // TODO: Store UI 구현
+            Text("Store Screen")
+        }
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subpage/store/StoreViewModel.kt
@@ -0,0 +1,7 @@
+package com.yong.taximeter.ui.main.subpage.store
+
+import cafe.adriel.voyager.core.model.ScreenModel
+
+class StoreViewModel: ScreenModel {
+    // TODO: ViewModel 로직 구현
+}
