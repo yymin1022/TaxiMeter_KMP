933908fa17b48e493e2035ca2fc6f46fb261cec9
yymin1022 <yymin1022@gmail.com>
2025-10-04 21:46:49 +0900

    Update NavigationTabs : Improved navigation content declaration

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/navigation/NavigationTabs.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/navigation/NavigationTabs.kt
@@ -7,7 +7,6 @@ import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
-import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.navigator.tab.Tab
 import cafe.adriel.voyager.navigator.tab.TabOptions
 import com.yong.taximeter.ui.main.subscreen.home.HomeScreen
@@ -40,9 +39,7 @@ internal object SettingTab : Tab {
 
     @Composable
     override fun Content() {
-        Navigator(
-            screen = SettingScreen
-        )
+        SettingScreen.Content()
     }
 }
 
@@ -67,9 +64,7 @@ internal object HomeTab: Tab {
 
     @Composable
     override fun Content() {
-        Navigator(
-            screen = HomeScreen
-        )
+        HomeScreen.Content()
     }
 }
 
@@ -94,8 +89,6 @@ internal object StoreTab : Tab {
 
     @Composable
     override fun Content() {
-        Navigator(
-            screen = StoreScreen
-        )
+        StoreScreen.Content()
     }
 }
