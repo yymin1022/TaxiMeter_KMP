3abc98af70318c04a433b4be4d4f093ee22c1847
yymin1022 <yymin1022@gmail.com>
2025-10-04 17:24:47 +0900

    Update Home : Added basic ui composables

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/home/HomeScreen.kt
@@ -1,9 +1,12 @@
 package com.yong.taximeter.ui.main.subscreen.home
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
@@ -26,11 +29,50 @@ object HomeScreen: Screen {
         modifier: Modifier = Modifier,
         uiState: HomeUiState
     ) {
-        Box(
-            modifier = modifier
+        // 전체 UI를 Column으로 배치
+        Column(
+            modifier = modifier,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
         ) {
-            // TODO: Home UI 구현
-            Text("Home Screen")
+            // Icon 영역
+            HomeIcon(
+                modifier = Modifier,
+            )
+
+            // Title 영역
+            HomeTitle(
+                modifier = Modifier,
+            )
+
+            // Description 영역
+            HomeTitleDescription(
+                modifier = Modifier,
+            )
         }
+    }
+
+    @Composable
+    private fun HomeIcon(
+        modifier: Modifier = Modifier
+    ) {
+        // TODO: Home Icon 구현
+        Text("Home Icon")
+    }
+
+    @Composable
+    private fun HomeTitle(
+        modifier: Modifier = Modifier
+    ) {
+        // TODO: Home Title 구현
+        Text("Home Title")
+    }
+
+    @Composable
+    private fun HomeTitleDescription(
+        modifier: Modifier = Modifier
+    ) {
+        // TODO: Home Title Description 구현
+        Text("Home Title Description")
     }
 }
