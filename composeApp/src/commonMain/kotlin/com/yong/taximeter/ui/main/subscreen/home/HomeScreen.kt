6b13548c29b040c0a163120e1ecac3938e574c7d
yymin1022 <yymin1022@gmail.com>
2025-10-04 17:37:55 +0900

    Update Home : Implemented HomeTitle

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/home/HomeScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/home/HomeScreen.kt
@@ -2,8 +2,8 @@ package com.yong.taximeter.ui.main.subscreen.home
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -13,9 +13,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
+import org.jetbrains.compose.resources.stringResource
+import taximeter.composeapp.generated.resources.Res
+import taximeter.composeapp.generated.resources.home_title_1
+import taximeter.composeapp.generated.resources.home_title_2
 
 object HomeScreen: Screen {
     @Composable
@@ -76,8 +82,23 @@ object HomeScreen: Screen {
     private fun HomeTitle(
         modifier: Modifier = Modifier
     ) {
-        // TODO: Home Title 구현
-        Text("Home Title")
+        val titleLeft = stringResource(Res.string.home_title_1)
+        val titleRight = stringResource(Res.string.home_title_2)
+
+        Row(
+            modifier = modifier,
+            horizontalArrangement = Arrangement.spacedBy(3.dp)
+        ) {
+            Text(
+                text = titleLeft,
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+            )
+            Text(
+                text = titleRight,
+                fontSize = 40.sp,
+            )
+        }
     }
 
     @Composable
