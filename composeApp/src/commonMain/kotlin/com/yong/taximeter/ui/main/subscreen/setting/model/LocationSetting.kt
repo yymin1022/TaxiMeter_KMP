6f251a6554840481275217c37538b4eb35ea842a
yymin1022 <yymin1022@gmail.com>
2025-10-05 17:23:09 +0900

    location setting

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/setting/model/LocationSetting.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/setting/model/LocationSetting.kt
@@ -1,41 +1,44 @@
 package com.yong.taximeter.ui.main.subscreen.setting.model
 
+import org.jetbrains.compose.resources.StringResource
 import taximeter.composeapp.generated.resources.Res
-import taximeter.composeapp.generated.resources.setting_item_location_seoul
-import taximeter.composeapp.generated.resources.setting_item_location_gangwon
-import taximeter.composeapp.generated.resources.setting_item_location_gyeonggi
-import taximeter.composeapp.generated.resources.setting_item_location_gyeongbuk
-import taximeter.composeapp.generated.resources.setting_item_location_gyeongnam
-import taximeter.composeapp.generated.resources.setting_item_location_gwangju
-import taximeter.composeapp.generated.resources.setting_item_location_daegu
-import taximeter.composeapp.generated.resources.setting_item_location_daejeon
 import taximeter.composeapp.generated.resources.setting_item_location_busan
-import taximeter.composeapp.generated.resources.setting_item_location_ulsan
-import taximeter.composeapp.generated.resources.setting_item_location_incheon
-import taximeter.composeapp.generated.resources.setting_item_location_jeonbuk
-import taximeter.composeapp.generated.resources.setting_item_location_jeonnam
-import taximeter.composeapp.generated.resources.setting_item_location_jeju
 import taximeter.composeapp.generated.resources.setting_item_location_chungbuk
 import taximeter.composeapp.generated.resources.setting_item_location_chungnam
 import taximeter.composeapp.generated.resources.setting_item_location_custom
-import org.jetbrains.compose.resources.StringResource
+import taximeter.composeapp.generated.resources.setting_item_location_daegu
+import taximeter.composeapp.generated.resources.setting_item_location_daejeon
+import taximeter.composeapp.generated.resources.setting_item_location_gangwon
+import taximeter.composeapp.generated.resources.setting_item_location_gwangju
+import taximeter.composeapp.generated.resources.setting_item_location_gyeongbuk
+import taximeter.composeapp.generated.resources.setting_item_location_gyeonggi
+import taximeter.composeapp.generated.resources.setting_item_location_gyeongnam
+import taximeter.composeapp.generated.resources.setting_item_location_incheon
+import taximeter.composeapp.generated.resources.setting_item_location_jeju
+import taximeter.composeapp.generated.resources.setting_item_location_jeonbuk
+import taximeter.composeapp.generated.resources.setting_item_location_jeonnam
+import taximeter.composeapp.generated.resources.setting_item_location_seoul
+import taximeter.composeapp.generated.resources.setting_item_location_ulsan
 
-enum class LocationSetting(val titleRes: StringResource) {
-    SEOUL(Res.string.setting_item_location_seoul),
-    GANGWON(Res.string.setting_item_location_gangwon),
-    GYEONGGI(Res.string.setting_item_location_gyeonggi),
-    GYEONGBUK(Res.string.setting_item_location_gyeongbuk),
-    GYEONGNAM(Res.string.setting_item_location_gyeongnam),
-    GWANGJU(Res.string.setting_item_location_gwangju),
-    DAEGU(Res.string.setting_item_location_daegu),
-    DAEJEON(Res.string.setting_item_location_daejeon),
-    BUSAN(Res.string.setting_item_location_busan),
-    ULSAN(Res.string.setting_item_location_ulsan),
-    INCHEON(Res.string.setting_item_location_incheon),
-    JEONBUK(Res.string.setting_item_location_jeonbuk),
-    JEONNAM(Res.string.setting_item_location_jeonnam),
-    JEJU(Res.string.setting_item_location_jeju),
-    CHUNGBUK(Res.string.setting_item_location_chungbuk),
-    CHUNGNAM(Res.string.setting_item_location_chungnam),
-    CUSTOM(Res.string.setting_item_location_custom),
+enum class LocationSetting(
+    val titleRes: StringResource,
+    val key: String,
+) {
+    SEOUL(Res.string.setting_item_location_seoul, "seoul"),
+    GANGWON(Res.string.setting_item_location_gangwon, "gangwon"),
+    GYEONGGI(Res.string.setting_item_location_gyeonggi, "gyeonggi"),
+    GYEONGBUK(Res.string.setting_item_location_gyeongbuk, "gyeongbuk"),
+    GYEONGNAM(Res.string.setting_item_location_gyeongnam, "gyeongnam"),
+    GWANGJU(Res.string.setting_item_location_gwangju, "gwangju"),
+    DAEGU(Res.string.setting_item_location_daegu, "daegu"),
+    DAEJEON(Res.string.setting_item_location_daejeon, "daejeon"),
+    BUSAN(Res.string.setting_item_location_busan, "busan"),
+    ULSAN(Res.string.setting_item_location_ulsan, "ulsan"),
+    INCHEON(Res.string.setting_item_location_incheon, "incheon"),
+    JEONBUK(Res.string.setting_item_location_jeonbuk, "jeonbuk"),
+    JEONNAM(Res.string.setting_item_location_jeonnam, "jeonnam"),
+    JEJU(Res.string.setting_item_location_jeju, "jeju"),
+    CHUNGBUK(Res.string.setting_item_location_chungbuk, "chungbuk"),
+    CHUNGNAM(Res.string.setting_item_location_chungnam, "chungnam"),
+    CUSTOM(Res.string.setting_item_location_custom, "custom");
 }
