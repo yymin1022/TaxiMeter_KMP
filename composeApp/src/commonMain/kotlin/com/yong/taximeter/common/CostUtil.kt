88c0f7e5c6ab50f825478cc0897b839597787f77
yymin1022 <yymin1022@gmail.com>
2025-10-05 18:35:14 +0900

    Added CostUtil for cost data management

---
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/CostUtil.kt
@@ -0,0 +1,12 @@
+package com.yong.taximeter.common
+
+object CostUtil {
+    private const val PREF_KEY_COST_DB_VERSION = "PREF_KEY_COST_DB_VERSION"
+
+    private const val DEFAULT_COST_DB_VERSION = "20001022"
+
+    suspend fun getCostDbVersion(): String {
+        val curVersion = PreferenceUtil.getString(PREF_KEY_COST_DB_VERSION, DEFAULT_COST_DB_VERSION)
+        return curVersion
+    }
+}
