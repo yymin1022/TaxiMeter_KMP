5ceb1f468af989bda315f601f5cf11aae5b63e34
yymin1022 <yymin1022@gmail.com>
2025-10-05 17:23:34 +0900

    theme setting

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/setting/model/ThemeSetting.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/main/subscreen/setting/model/ThemeSetting.kt
@@ -5,7 +5,10 @@ import taximeter.composeapp.generated.resources.Res
 import taximeter.composeapp.generated.resources.setting_item_theme_circle
 import taximeter.composeapp.generated.resources.setting_item_theme_horse
 
-enum class ThemeSetting(val titleRes: StringResource) {
-    CIRCLE(Res.string.setting_item_theme_circle),
-    HORSE(Res.string.setting_item_theme_horse),
+enum class ThemeSetting(
+    val titleRes: StringResource,
+    val key: String,
+) {
+    CIRCLE(Res.string.setting_item_theme_circle, "circle"),
+    HORSE(Res.string.setting_item_theme_horse, "horse"),
 }
