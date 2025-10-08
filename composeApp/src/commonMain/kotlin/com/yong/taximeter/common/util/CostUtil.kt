588ab6f55d07ca8afca5a9b0652ded6f7f30ace6
yymin1022 <yymin1022@gmail.com>
2025-10-09 01:36:21 +0900

    Update Cost Util : Added function for check update

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/common/util/CostUtil.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/util/CostUtil.kt
@@ -1,12 +1,36 @@
 package com.yong.taximeter.common.util
 
+import dev.gitlive.firebase.Firebase
+import dev.gitlive.firebase.firestore.firestore
+
 object CostUtil {
     private const val PREF_KEY_COST_DB_VERSION = "PREF_KEY_COST_DB_VERSION"
 
     private const val DEFAULT_COST_DB_VERSION = "20001022"
 
+    private val firestore = Firebase.firestore
+
+    /**
+     * Get local Cost DB Version
+     */
     suspend fun getCostDbVersion(): String {
         val curVersion = PreferenceUtil.getString(PREF_KEY_COST_DB_VERSION, DEFAULT_COST_DB_VERSION)
         return curVersion
+    }
+
+    /**
+     * Check if DB Update is available
+     */
+    suspend fun isUpdateAvailable(): Boolean {
+        return try {
+            val localVersion = getCostDbVersion()
+            val remoteVersionDoc = firestore.collection("cost").document("version").get()
+            val remoteVersion = remoteVersionDoc.get<String>("data")
+
+            localVersion != remoteVersion
+        } catch(e: Exception) {
+            e.printStackTrace()
+            false
+        }
     }
 }
