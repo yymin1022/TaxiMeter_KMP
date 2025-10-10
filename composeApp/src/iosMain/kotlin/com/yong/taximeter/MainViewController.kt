696818de966d1d81fc9e21c45b0510af27fbb231
yymin1022 <yymin1022@gmail.com>
2025-10-10 17:13:58 +0900

    Update Project : Added RevenueCat init for iOS

--- composeApp/src/iosMain/kotlin/com/yong/taximeter/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/yong/taximeter/MainViewController.kt
@@ -1,12 +1,19 @@
 package com.yong.taximeter
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.revenuecat.purchases.kmp.Purchases
+import com.revenuecat.purchases.kmp.PurchasesConfiguration
 import com.yong.taximeter.di.appModule
 import com.yong.taximeter.di.platformModule
 import org.koin.core.context.startKoin
+import secrets.Secrets
 
 fun MainViewController() = ComposeUIViewController(
     configure = {
+        // RevenueCat Init
+        val builder = PurchasesConfiguration.Builder(Secrets.REVENUECAT_API_KEY_IOS)
+        Purchases.configure(builder.build())
+
         startKoin {
             modules(appModule, platformModule)
         }
