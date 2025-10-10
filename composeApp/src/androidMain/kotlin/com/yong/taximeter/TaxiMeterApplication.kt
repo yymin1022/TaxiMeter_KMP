f5443bc012dcf9af3835dce4ec3079dbe7c04180
yymin1022 <yymin1022@gmail.com>
2025-10-10 17:13:47 +0900

    Update Project : Added RevenueCat init for Android

--- composeApp/src/androidMain/kotlin/com/yong/taximeter/TaxiMeterApplication.kt
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/TaxiMeterApplication.kt
@@ -1,18 +1,27 @@
 package com.yong.taximeter
 
 import android.app.Application
+import com.google.firebase.BuildConfig
+import com.revenuecat.purchases.kmp.Purchases
+import com.revenuecat.purchases.kmp.PurchasesConfiguration
 import com.yong.taximeter.di.appModule
 import com.yong.taximeter.di.platformModule
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.initialize
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import secrets.Secrets
+import javax.crypto.SecretKey
 
 class TaxiMeterApplication: Application() {
     override fun onCreate() {
         super.onCreate()
 
         Firebase.initialize(this)
+
+        // RevenueCat Init
+        val builder = PurchasesConfiguration.Builder(Secrets.REVENUECAT_API_KEY_ANDROID)
+        Purchases.configure(builder.build())
 
         startKoin {
             // Common App Module init
