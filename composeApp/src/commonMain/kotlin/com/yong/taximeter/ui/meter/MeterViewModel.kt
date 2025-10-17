324fc498fb6fd3724e9f106f6d9e8146bd118c0d
yymin1022 <yymin1022@gmail.com>
2025-10-18 01:55:55 +0900

    Update Meter ViewModel : Defined UI State for Meter

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterViewModel.kt
@@ -1,7 +1,23 @@
 package com.yong.taximeter.ui.meter
 
 import cafe.adriel.voyager.core.model.ScreenModel
+import com.yong.taximeter.common.model.CostMode
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class MeterUiState(
+    val curCost: Int = 0,
+    val curCoseMode: CostMode = CostMode.MODE_BASE,
+    val curCounter: Int = 0,
+    val curDistance: Float = 0f,
+    val curSpeed: Float = 0f,
+
+    val isDriving: Boolean = false,
+    val isNightPerc: Boolean = false,
+    val isOutCityPerc: Boolean = false,
+)
 
 class MeterViewModel: ScreenModel {
-    // TODO: ViewModel 로직 구현
+    private val _uiState = MutableStateFlow(MeterUiState())
+    val uiState = _uiState.asStateFlow()
 }
