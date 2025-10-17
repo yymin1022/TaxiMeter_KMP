26d9bcb4e190a0103eb6ca2977150e258bdd3074
yymin1022 <yymin1022@gmail.com>
2025-10-18 02:44:59 +0900

    Update Meter ViewModel : Added cost info initialization

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterViewModel.kt
@@ -1,10 +1,17 @@
 package com.yong.taximeter.ui.meter
 
 import cafe.adriel.voyager.core.model.ScreenModel
+import cafe.adriel.voyager.core.model.screenModelScope
+import com.yong.taximeter.common.model.CostInfo
 import com.yong.taximeter.common.model.CostMode
+import com.yong.taximeter.common.util.CostUtil
+import com.yong.taximeter.common.util.PreferenceUtil
+import com.yong.taximeter.common.util.PreferenceUtil.KEY_SETTING_LOCATION
+import com.yong.taximeter.ui.main.subscreen.setting.model.LocationSetting
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 data class MeterUiState(
     val curCost: Int = 0,
@@ -22,6 +29,18 @@ class MeterViewModel: ScreenModel {
     private val _uiState = MutableStateFlow(MeterUiState())
     val uiState = _uiState.asStateFlow()
 
+    private lateinit var costInfo: CostInfo
+
+    init {
+        screenModelScope.launch {
+            val curLocationPref = PreferenceUtil.getString(KEY_SETTING_LOCATION, "")
+            val curLocation = LocationSetting.fromKey(curLocationPref)
+            this@MeterViewModel.costInfo = CostUtil.getCostForLocation(curLocation)
+
+            setupCostState()
+        }
+    }
+
     fun startDriving() {
         _uiState.update { it.copy(isDriving = true) }
     }
@@ -36,5 +55,20 @@ class MeterViewModel: ScreenModel {
 
     fun updateOutCityPerc(isEnabled: Boolean) {
         _uiState.update { it.copy(isOutCityPerc = isEnabled) }
+    }
+
+    private fun setupCostState() {
+        _uiState.update {
+            it.copy(
+                curCost = this.costInfo.costBase,
+                curCostMode = CostMode.MODE_BASE,
+                curCounter = this.costInfo.distBase,
+                curDistance = 0f,
+                curSpeed = 0f,
+                isDriving = false,
+                isNightPerc = false,
+                isOutCityPerc = false,
+            )
+        }
     }
 }
