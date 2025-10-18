47758dd7c2e2665561ab18bae8bb23f3086bad79
yymin1022 <yymin1022@gmail.com>
2025-10-18 17:14:41 +0900

    Update Meter ViewModel : Implemented Cost Increasing Logic for Drive

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterViewModel.kt
@@ -8,10 +8,17 @@ import com.yong.taximeter.common.util.CostUtil
 import com.yong.taximeter.common.util.PreferenceUtil
 import com.yong.taximeter.common.util.PreferenceUtil.KEY_SETTING_LOCATION
 import com.yong.taximeter.ui.main.subscreen.setting.model.LocationSetting
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+import kotlin.time.ExperimentalTime
 
 data class MeterUiState(
     val curCost: Int = 0,
@@ -26,10 +33,20 @@ data class MeterUiState(
 )
 
 class MeterViewModel: ScreenModel {
+    companion object {
+        private const val METER_SPEED_COST_THRESHOLD_KMH = 15f
+
+        private const val METER_UPDATE_INTERVAL = 1000L
+        private const val METER_UPDATE_NEED_INIT = -1L
+    }
+
     private val _uiState = MutableStateFlow(MeterUiState())
     val uiState = _uiState.asStateFlow()
 
     private lateinit var costInfo: CostInfo
+
+    private var meterDriveJob: Job? = null
+    private var lastUpdateTimeMillis: Long = -1L
 
     init {
         screenModelScope.launch {
@@ -37,16 +54,23 @@ class MeterViewModel: ScreenModel {
             val curLocation = LocationSetting.fromKey(curLocationPref)
             this@MeterViewModel.costInfo = CostUtil.getCostForLocation(curLocation)
 
-            setupCostState()
+            setCostInitialState()
         }
     }
 
     fun startDriving() {
-        _uiState.update { it.copy(isDriving = true) }
+        if(uiState.value.isDriving.not()) {
+            startDriveJob()
+            _uiState.update { it.copy(isDriving = true) }
+        }
     }
 
     fun stopDriving() {
-        _uiState.update { it.copy(isDriving = false) }
+        if(uiState.value.isDriving) {
+            meterDriveJob?.cancel()
+            setCostInitialState()
+        }
+
     }
 
     fun updateNightPerc(isEnabled: Boolean) {
@@ -57,7 +81,9 @@ class MeterViewModel: ScreenModel {
         _uiState.update { it.copy(isOutCityPerc = isEnabled) }
     }
 
-    private fun setupCostState() {
+    private fun setCostInitialState() {
+        lastUpdateTimeMillis = METER_UPDATE_NEED_INIT
+
         _uiState.update {
             it.copy(
                 curCost = this.costInfo.costBase,
@@ -68,6 +94,144 @@ class MeterViewModel: ScreenModel {
                 isDriving = false,
                 isNightPerc = false,
                 isOutCityPerc = false,
+            )
+        }
+    }
+
+    private fun startDriveJob() {
+        this.meterDriveJob = screenModelScope.launch(Dispatchers.Default) {
+            while(true) {
+                increaseCost()
+                delay(METER_UPDATE_INTERVAL)
+            }
+        }
+    }
+
+    @OptIn(ExperimentalTime::class)
+    private fun increaseCost() {
+        // 현재 SystemTime Instant
+        val curMoment = Clock.System.now()
+        val curTimeMillis = curMoment.toEpochMilliseconds()
+
+        // 아직 Last UpdateTime이 초기화되지 않았다면,
+        // 첫 Update이므로 요금 관련해서는 아무 작업도 하지 않는다
+        if(lastUpdateTimeMillis == METER_UPDATE_NEED_INIT) {
+            lastUpdateTimeMillis = curTimeMillis
+            return
+        }
+
+        // Delta Time 계산
+        val deltaTime = (curTimeMillis - lastUpdateTimeMillis).toFloat() / 1000f
+
+        // 현재 GPS Speed 확인
+        // TODO: GPS 관련 구현 후 실제 현재 속도를 m/s 단위로 받아오도록 변경
+        // TODO: GPS 정확도 관련 API Check 후 UI 예외처리 필요
+        val curGpsSpeed = 0f
+        val newSpeed = curGpsSpeed * 3.6f
+
+        // 이동 거리 Update
+        val curDistance = uiState.value.curDistance
+        val deltaDistance = curGpsSpeed * deltaTime
+        val newDistance = curDistance + (deltaDistance / 1000f)
+
+        // CostMode
+        val curCostMode = uiState.value.curCostMode
+        var newCostMode = curCostMode
+
+        // Cost 연산에 필요한 기준 값 (거리요금 / 시간요금)
+        val costRunPer = costInfo.costRunPer
+        val costTimePer = costInfo.costTimePer
+
+        // Cost Counter 변화 연산
+        val curCounter = uiState.value.curCounter
+        var newCounter = curCounter - deltaDistance.toInt()
+        if(newSpeed <= METER_SPEED_COST_THRESHOLD_KMH) {
+            // 시간요금 기준 속도보다 느린 경우
+            newCounter -= (costRunPer.toFloat() / costTimePer.toFloat() * deltaTime).toInt()
+
+            // 기본요금이 아닌 거리요금 주행 중인 경우 거리요금으로 주행모드 변경
+            if(curCostMode == CostMode.MODE_DISTANCE) {
+                newCostMode = CostMode.MODE_TIME
+            }
+        } else {
+            // 시간요금 기준 속도보다 빠르며,
+            // 기본요금이 아닌 시간요금 주행 중인 경우 거리요금으로 주행모드 변경
+            if(curCostMode == CostMode.MODE_TIME) {
+                newCostMode = CostMode.MODE_DISTANCE
+            }
+        }
+
+        // Cost 변화 연산
+        val curCost = uiState.value.curCost
+        var newCost = curCost
+        if(newCounter <= 0) {
+            // Cost 정보
+            var deltaCost = 100
+
+            // 기본요금 주행 중인 경우, 주행 모드 변경
+            if(curCostMode == CostMode.MODE_BASE) {
+                newCostMode = if(newSpeed <= METER_SPEED_COST_THRESHOLD_KMH)
+                                CostMode.MODE_TIME
+                            else
+                                CostMode.MODE_DISTANCE
+            }
+
+            // Counter 초기화
+            newCounter += costRunPer
+            // 주행 속도가 매우 빠른 경우 음수 Counter가 발생할 수 있으므로 0으로 최소값 제한
+            newCounter = newCounter.coerceAtLeast(0)
+
+            // 심야할증 적용
+            val isNightPerc = uiState.value.isNightPerc
+            // TODO: 심야할증은 직접 적용하지 않고, 시간대에 따라 자동 적용되도록 변경 필요
+            if(isNightPerc) {
+                val curTimeZone = TimeZone.currentSystemDefault()
+                val curDateTime = curMoment.toLocalDateTime(curTimeZone)
+
+                // 심야할증 적용 연산은 1단계 적용 여부 확인 후,
+                // 2단계 여부를 확인해 최종 연산한다
+                val curHour = curDateTime.hour
+                val nightPerc1From = costInfo.percNight1From
+                val nightPerc1To = costInfo.percNight1To
+                if((curHour >= 20 && curHour >= nightPerc1From)
+                    || (curHour <= 6 && curHour < nightPerc1To)) {
+                    // 심야할증 1단계 적용 가능
+                    val nightPerc2From = costInfo.percNight2From
+                    val nightPerc2To = costInfo.percNight2To
+                    if((curHour >= 20 && curHour >= nightPerc2From)
+                        || (curHour <= 6 && curHour < nightPerc2To)) {
+                        // 심야할증 2단계 적용
+                        val percNight2 = costInfo.percNight2
+                        deltaCost += percNight2
+                    } else {
+                        // 심야할증 1단계 적용
+                        val percNight1 = costInfo.percNight1
+                        deltaCost += percNight1
+                    }
+                    // TODO: 심야할증 활성화 UI State Update
+                } else {
+                    // TODO: 심야할증 비활성화 UI State Update
+                }
+            }
+
+            // 시외할증 적용
+            val isOutCityPerc = uiState.value.isOutCityPerc
+            if(isOutCityPerc) {
+                val percOutCity = costInfo.percCity
+                deltaCost += percOutCity
+            }
+
+            newCost = curCost + deltaCost
+        }
+
+        lastUpdateTimeMillis = curTimeMillis
+        _uiState.update {
+            it.copy(
+                curCost = newCost,
+                curCostMode = newCostMode,
+                curCounter = newCounter,
+                curDistance = newDistance,
+                curSpeed = newSpeed,
             )
         }
     }
