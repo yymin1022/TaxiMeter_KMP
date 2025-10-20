e283231323c486480ff8e522679b50c60c408ac1
yymin1022 <yymin1022@gmail.com>
2025-10-20 20:06:00 +0900

    Update CostMode : Added parameter for String Resources

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/common/model/CostMode.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/model/CostMode.kt
@@ -1,7 +1,13 @@
 package com.yong.taximeter.common.model
 
-enum class CostMode {
-    MODE_BASE,
-    MODE_DISTANCE,
-    MODE_TIME,
+import org.jetbrains.compose.resources.StringResource
+import taximeter.composeapp.generated.resources.Res
+import taximeter.composeapp.generated.resources.meter_cost_mode_base
+import taximeter.composeapp.generated.resources.meter_cost_mode_distance
+import taximeter.composeapp.generated.resources.meter_cost_mode_time
+
+enum class CostMode(val msgRes: StringResource) {
+    MODE_BASE(msgRes = Res.string.meter_cost_mode_base),
+    MODE_DISTANCE(msgRes = Res.string.meter_cost_mode_distance),
+    MODE_TIME(msgRes = Res.string.meter_cost_mode_time),
 }
