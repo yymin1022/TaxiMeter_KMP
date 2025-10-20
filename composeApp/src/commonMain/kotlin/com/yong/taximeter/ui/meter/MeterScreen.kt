acfcfda4f92912b7f55560c4f1d90dbf26bd8893
yymin1022 <yymin1022@gmail.com>
2025-10-20 20:59:48 +0900

    Update Meter : Implemented UI - Meter Status TextView

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterScreen.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/ui/meter/MeterScreen.kt
@@ -1,10 +1,14 @@
 package com.yong.taximeter.ui.meter
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
@@ -14,13 +18,28 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.koin.getScreenModel
 import com.yong.taximeter.common.model.CostMode
 import com.yong.taximeter.common.ui.ShowSnackBar
 import org.jetbrains.compose.resources.DrawableResource
+import org.jetbrains.compose.resources.StringResource
+import org.jetbrains.compose.resources.stringResource
+import taximeter.composeapp.generated.resources.Res
+import taximeter.composeapp.generated.resources.meter_status_cost_mode_title
+import taximeter.composeapp.generated.resources.meter_status_distance_desc
+import taximeter.composeapp.generated.resources.meter_status_distance_title
+import taximeter.composeapp.generated.resources.meter_status_drive_status_desc_false
+import taximeter.composeapp.generated.resources.meter_status_drive_status_desc_true
+import taximeter.composeapp.generated.resources.meter_status_drive_status_title
+import taximeter.composeapp.generated.resources.meter_status_speed_desc
+import taximeter.composeapp.generated.resources.meter_status_speed_title
+import kotlin.math.round
 
 object MeterScreen: Screen {
     @Composable
@@ -44,6 +63,7 @@ object MeterScreen: Screen {
             MeterScreenInternal(
                 modifier = Modifier
                     .fillMaxSize()
+                    .background(color = Color.Black)
                     .padding(16.dp),
                 uiState = uiState.value,
                 startDriving = viewModel::startDriving,
@@ -145,23 +165,93 @@ object MeterScreen: Screen {
         curSpeed: Float,
         isDriving: Boolean,
     ) {
-        // TODO: Meter Status UI 구현
-        Column(
-            modifier = modifier,
+        val curDistanceRound = round(curDistance * 10) / 10
+        val curSpeedRound = round(curSpeed * 10) / 10
+
+        val costModeTitle = stringResource(Res.string.meter_status_cost_mode_title)
+        val costModeDesc = stringResource(curCostMode.msgRes)
+        val distanceTitle = stringResource(Res.string.meter_status_distance_title)
+        val distanceDesc = stringResource(Res.string.meter_status_distance_desc, curDistanceRound)
+        val driveStatusTitle = stringResource(Res.string.meter_status_drive_status_title)
+        val driveStatusDesc = stringResource(
+            if(isDriving) Res.string.meter_status_drive_status_desc_true
+                    else Res.string.meter_status_drive_status_desc_false)
+        val speedTitle = stringResource(Res.string.meter_status_speed_title)
+        val speedDesc = stringResource(Res.string.meter_status_speed_desc, curSpeedRound)
+
+        Row(
+            modifier = modifier
+                .fillMaxWidth(),
         ) {
-            Row(
-                modifier = Modifier,
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 12.dp),
             ) {
-                Text("Cost Mode [$curCostMode]")
-                Text("Driving Status [$isDriving]")
+                // Drive Status
+                MeterStatusText(
+                    modifier = Modifier,
+                    title = driveStatusTitle,
+                    desc = driveStatusDesc,
+                    isLeftAlign = false,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                // Cost Mode Status
+                MeterStatusText(
+                    modifier = Modifier,
+                    title = costModeTitle,
+                    desc = costModeDesc,
+                    isLeftAlign = false,
+                )
             }
 
-            Row(
-                modifier = Modifier,
+            Column(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 12.dp),
             ) {
-                Text("Speed [$curSpeed]")
-                Text("Distance [$curDistance]")
+                // Speed Status
+                MeterStatusText(
+                    modifier = Modifier,
+                    title = speedTitle,
+                    desc = speedDesc,
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                // Distance Status
+                MeterStatusText(
+                    modifier = Modifier,
+                    title = distanceTitle,
+                    desc = distanceDesc,
+                )
             }
+        }
+    }
+
+    @Composable
+    private fun MeterStatusText(
+        modifier: Modifier = Modifier,
+        title: String,
+        desc: String,
+        isLeftAlign: Boolean = true,
+    ) {
+        Column(
+            modifier = modifier
+                .fillMaxWidth(),
+            horizontalAlignment = if(isLeftAlign) Alignment.Start else Alignment.End,
+        ) {
+            // Title Text
+            Text(
+                text = title,
+                color = Color.LightGray,
+                fontSize = 20.sp,
+            )
+
+            // Description Text
+            Text(
+                text = desc,
+                color = Color.LightGray,
+                fontSize = 20.sp,
+            )
         }
     }
 
