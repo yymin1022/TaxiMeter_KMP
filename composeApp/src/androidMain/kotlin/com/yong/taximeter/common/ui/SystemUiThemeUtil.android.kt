77121d56989af85482244b83aef0c727511d273f
yymin1022 <yymin1022@gmail.com>
2025-10-26 18:08:01 +0900

    Added SystemUiTheme Util

---
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.android.kt
@@ -0,0 +1,7 @@
+package com.yong.taximeter.common.ui
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual object SystemUiThemeUtil {
+    actual fun setSystemUiTheme(isDark: Boolean) {
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.kt
@@ -0,0 +1,6 @@
+package com.yong.taximeter.common.ui
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect object SystemUiThemeUtil {
+    fun setSystemUiTheme(isDark: Boolean)
+}
