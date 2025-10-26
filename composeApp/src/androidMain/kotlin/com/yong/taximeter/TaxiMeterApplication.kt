cb37f16ca1703d756763d6fc64617acbd04441d2
yymin1022 <yymin1022@gmail.com>
2025-10-26 19:10:30 +0900

    Update Project : Implemented SystemUiTheme Util for Android

--- composeApp/src/androidMain/kotlin/com/yong/taximeter/TaxiMeterApplication.kt
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/TaxiMeterApplication.kt
@@ -1,6 +1,8 @@
 package com.yong.taximeter
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import com.revenuecat.purchases.kmp.Purchases
 import com.revenuecat.purchases.kmp.PurchasesConfiguration
 import com.yong.taximeter.di.appModule
@@ -10,10 +12,35 @@ import dev.gitlive.firebase.initialize
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import secrets.Secrets
+import java.lang.ref.WeakReference
 
 class TaxiMeterApplication: Application() {
+    companion object {
+        var currentActivity: WeakReference<Activity>? = null
+    }
+
     override fun onCreate() {
         super.onCreate()
+
+        registerActivityLifecycleCallbacks(object : ActivityLifecycleCallbacks {
+            override fun onActivityCreated(activity: Activity, bundle: Bundle?) {}
+            override fun onActivityStarted(activity: Activity) {}
+            override fun onActivityResumed(activity: Activity) {
+                currentActivity = WeakReference(activity)
+            }
+            override fun onActivityPaused(activity: Activity) {
+                if(currentActivity?.get() == activity) {
+                    currentActivity = null
+                }
+            }
+            override fun onActivityStopped(activity: Activity) {}
+            override fun onActivitySaveInstanceState(activity: Activity, bundle: Bundle) {}
+            override fun onActivityDestroyed(activity: Activity) {
+                if(currentActivity?.get() == activity) {
+                    currentActivity = null
+                }
+            }
+        })
 
         Firebase.initialize(this)
 
--- composeApp/src/androidMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.android.kt
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.android.kt
@@ -1,7 +1,18 @@
 package com.yong.taximeter.common.ui
 
+import androidx.core.view.WindowCompat
+import com.yong.taximeter.TaxiMeterApplication
+
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object SystemUiThemeUtil {
     actual fun setSystemUiTheme(isDark: Boolean) {
+        val activity = TaxiMeterApplication.currentActivity?.get() ?: return
+
+        val window = activity.window ?: return
+        val view = window.decorView
+        val insetsController = WindowCompat.getInsetsController(window, view)
+
+        insetsController.isAppearanceLightStatusBars = !isDark
+        insetsController.isAppearanceLightNavigationBars = !isDark
     }
 }
