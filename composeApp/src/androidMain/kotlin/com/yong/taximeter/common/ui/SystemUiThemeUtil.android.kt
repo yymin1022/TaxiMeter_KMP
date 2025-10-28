706f5c907b9a802a4627d602e7826ba58f136fd0
yymin1022 <yymin1022@gmail.com>
2025-10-28 20:24:48 +0900

    Update SystemUiTheme Util : Removed legacy of Android Implementation

--- composeApp/src/androidMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.android.kt
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.android.kt
@@ -1,20 +1,11 @@
 package com.yong.taximeter.common.ui
 
 import androidx.compose.runtime.Composable
-import androidx.core.view.WindowCompat
-import com.yong.taximeter.TaxiMeterApplication
 
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object SystemUiThemeUtil {
     @Composable
     actual fun setSystemUiTheme(isDark: Boolean) {
-        val activity = TaxiMeterApplication.currentActivity?.get() ?: return
 
-        val window = activity.window ?: return
-        val view = window.decorView
-        val insetsController = WindowCompat.getInsetsController(window, view)
-
-        insetsController.isAppearanceLightStatusBars = !isDark
-        insetsController.isAppearanceLightNavigationBars = !isDark
     }
 }
