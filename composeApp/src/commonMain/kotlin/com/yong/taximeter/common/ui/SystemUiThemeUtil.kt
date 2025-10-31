bf5d1cfff48008ed32cbf0997e60a35fd5b61454
yymin1022 <yymin1022@gmail.com>
2025-10-31 15:33:46 +0900

    Update SystemUiTheme Util : Migrated as composable remember unit

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.kt
@@ -5,5 +5,5 @@ import androidx.compose.runtime.Composable
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 expect object SystemUiThemeUtil {
     @Composable
-    fun setSystemUiTheme(isDark: Boolean)
+    fun rememberSystemUiThemeSetter(): (isDark: Boolean) -> Unit
 }
--- composeApp/src/iosMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.ios.kt
+++ composeApp/src/iosMain/kotlin/com/yong/taximeter/common/ui/SystemUiThemeUtil.ios.kt
@@ -1,6 +1,7 @@
 package com.yong.taximeter.common.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import platform.UIKit.NSForegroundColorAttributeName
 import platform.UIKit.UIApplication
 import platform.UIKit.UIColor
@@ -12,23 +13,27 @@ import platform.UIKit.setStatusBarStyle
 @Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
 actual object SystemUiThemeUtil {
     @Composable
-    actual fun setSystemUiTheme(isDark: Boolean) {
-        // StatusBar 색상 지정
-        // - [isDark]에 따라 Default / Light
-        val statusBarStyle =
-            if(isDark) UIStatusBarStyleLightContent
-            else UIStatusBarStyleDefault
-        UIApplication.sharedApplication.setStatusBarStyle(statusBarStyle)
+    actual fun rememberSystemUiThemeSetter(): (isDark: Boolean) -> Unit {
+        return remember {
+            { isDark ->
+                // StatusBar 색상 지정
+                // - [isDark]에 따라 Default / Light
+                val statusBarStyle =
+                    if(isDark) UIStatusBarStyleLightContent
+                    else UIStatusBarStyleDefault
+                UIApplication.sharedApplication.setStatusBarStyle(statusBarStyle)
 
-        // NavigationBar 색상 지정
-        val navBarBackgroundColor = if(isDark) UIColor.blackColor else UIColor.whiteColor
-        val navBarIconColor = if(isDark) UIColor.whiteColor else UIColor.blackColor
-        val navBar = UINavigationBar.appearance().apply {
-            barTintColor = navBarBackgroundColor
-            tintColor = navBarIconColor
-            titleTextAttributes = mapOf<Any?, Any?>(
-                NSForegroundColorAttributeName to navBarIconColor
-            )
+                // NavigationBar 색상 지정
+                val navBarBackgroundColor = if(isDark) UIColor.blackColor else UIColor.whiteColor
+                val navBarIconColor = if(isDark) UIColor.whiteColor else UIColor.blackColor
+                val navBar = UINavigationBar.appearance().apply {
+                    barTintColor = navBarBackgroundColor
+                    tintColor = navBarIconColor
+                    titleTextAttributes = mapOf<Any?, Any?>(
+                        NSForegroundColorAttributeName to navBarIconColor
+                    )
+                }
+            }
         }
     }
 }
