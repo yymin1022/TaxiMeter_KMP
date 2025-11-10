4b53740dae2a4576cfc9257ed5b1780232c0e198
yymin1022 <yymin1022@gmail.com>
2025-11-10 23:29:21 +0900

    Added LocationUtil

---
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/common/util/LocationUtil.android.kt
@@ -0,0 +1,9 @@
+package com.yong.taximeter.common.util
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+actual object LocationUtil {
+    actual fun getCurrentSpeed(): Float {
+        // TODO: Android 로직 구현
+        return 0f
+    }
+}
---
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/util/LocationUtil.kt
@@ -0,0 +1,6 @@
+package com.yong.taximeter.common.util
+
+@Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+expect object LocationUtil {
+    fun getCurrentSpeed(): Float
+}
