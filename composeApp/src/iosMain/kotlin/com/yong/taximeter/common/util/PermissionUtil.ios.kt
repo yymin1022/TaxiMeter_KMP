acc69967a9fdd567be5e2d9b52e793ba123f16d5
yymin1022 <yymin1022@gmail.com>
2025-11-24 21:13:58 +0900

    Update PermissionUtil : Implemented Location Permission Checking for iOS Module

--- composeApp/src/iosMain/kotlin/com/yong/taximeter/common/util/PermissionUtil.ios.kt
+++ composeApp/src/iosMain/kotlin/com/yong/taximeter/common/util/PermissionUtil.ios.kt
@@ -1,10 +1,22 @@
 package com.yong.taximeter.common.util
 
+import platform.CoreLocation.CLLocationManager
+import platform.CoreLocation.kCLAuthorizationStatusAuthorizedAlways
+import platform.CoreLocation.kCLAuthorizationStatusAuthorizedWhenInUse
+
 @Suppress(names = ["EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING"])
 actual object PermissionUtil {
     actual fun isLocationPermissionGranted(): Boolean {
-        // TODO: iOS 위치정보 권한 여부 확인
-        return false
+        // CLLocationManager의 상태 확인
+        val authStatus = CLLocationManager.authorizationStatus()
+        // 상태 값에 따라 권한 부여 여부 판단
+        val isGranted = when(authStatus) {
+            kCLAuthorizationStatusAuthorizedWhenInUse,
+            kCLAuthorizationStatusAuthorizedAlways -> true
+            else -> false
+        }
+
+        return isGranted
     }
 
     actual fun requestLocationPermission() {
