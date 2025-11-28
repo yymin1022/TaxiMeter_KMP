a19cb3fd188da14577c2e3ab87092a19532bca21
yymin1022 <yymin1022@gmail.com>
2025-11-28 18:02:13 +0900

    Update Android Module : Added ActivityProvider Setup

--- composeApp/src/androidMain/kotlin/com/yong/taximeter/TaxiMeterApplication.kt
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/TaxiMeterApplication.kt
@@ -1,16 +1,22 @@
 package com.yong.taximeter
 
+import android.app.Activity
 import android.app.Application
+import android.os.Bundle
 import com.revenuecat.purchases.kmp.Purchases
 import com.revenuecat.purchases.kmp.PurchasesConfiguration
 import com.yong.taximeter.common.manager.LocationManagerFactory
 import com.yong.taximeter.common.util.PermissionUtil
+import com.yong.taximeter.di.ActivityProvider
+import com.yong.taximeter.di.ActivityProviderImpl
+import com.yong.taximeter.di.androidModule
 import com.yong.taximeter.di.appModule
 import com.yong.taximeter.di.platformModule
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.initialize
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import org.koin.java.KoinJavaComponent.get
 import secrets.Secrets
 
 class TaxiMeterApplication: Application() {
@@ -31,8 +37,29 @@ class TaxiMeterApplication: Application() {
         startKoin {
             // Common App Module init
             androidContext(this@TaxiMeterApplication)
-            modules(appModule, platformModule)
+            modules(androidModule, appModule, platformModule)
         }
+
+        // Activity Provider 초기화
+        val activityProvider = get<ActivityProvider>(ActivityProviderImpl::class.java)
+        registerActivityLifecycleCallbacks(object: ActivityLifecycleCallbacks {
+            // Activity Start, Stop 기준으로 Provider 업데이트 호출
+            override fun onActivityStarted(activity: Activity) {
+                activityProvider.setCurrentActivity(activity)
+            }
+
+            override fun onActivityStopped(activity: Activity) {
+                if(activityProvider.getCurrentActivity() === activity) {
+                    activityProvider.setCurrentActivity(null)
+                }
+            }
+
+            override fun onActivityCreated(a: Activity, b: Bundle?) {}
+            override fun onActivityDestroyed(a: Activity) {}
+            override fun onActivityPaused(a: Activity) {}
+            override fun onActivityResumed(a: Activity) {}
+            override fun onActivitySaveInstanceState(a: Activity, b: Bundle) {}
+        })
     }
 
     override fun onLowMemory() {
