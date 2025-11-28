1ef0e854a8866b6994ecb564e3924d2883177c0d
yymin1022 <yymin1022@gmail.com>
2025-11-28 19:00:11 +0900

    Update PermissionUtil : Implemented Location Permission Request Logic for Android Module

--- composeApp/src/androidMain/kotlin/com/yong/taximeter/common/util/PermissionUtil.android.kt
+++ composeApp/src/androidMain/kotlin/com/yong/taximeter/common/util/PermissionUtil.android.kt
@@ -3,13 +3,20 @@ package com.yong.taximeter.common.util
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.yong.taximeter.di.ActivityProvider
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 import kotlin.getValue
 
 @Suppress(names = ["EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING"])
 actual object PermissionUtil: KoinComponent {
+    // Activity Result Code
+    private const val REQUEST_CODE_LOCATION = 1001
+
+    // Activity Provider
+    private val activityProvider: ActivityProvider by inject()
     // Android Context
     private val context: Context by inject()
 
@@ -24,6 +31,14 @@ actual object PermissionUtil: KoinComponent {
     }
 
     actual fun requestLocationPermission() {
-        // TODO: Android 위치정보 권한 요청
+        val activity = activityProvider.getCurrentActivity() ?: return
+        // 이미 권한이 있으면 굳이 다시 요청할 필요 없음
+        if (isLocationPermissionGranted()) return
+
+        ActivityCompat.requestPermissions(
+            activity,
+            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+            REQUEST_CODE_LOCATION
+        )
     }
 }
