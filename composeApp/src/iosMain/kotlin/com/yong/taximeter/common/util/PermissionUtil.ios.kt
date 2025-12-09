9c5259184a130254d1196216945c09a82c70f0a1
yymin1022 <yymin1022@gmail.com>
2025-12-09 23:07:58 +0900

    Update PermissionUtil: Improved LocationManager declaration for iOS Module

--- composeApp/src/iosMain/kotlin/com/yong/taximeter/common/util/PermissionUtil.ios.kt
+++ composeApp/src/iosMain/kotlin/com/yong/taximeter/common/util/PermissionUtil.ios.kt
@@ -6,9 +6,12 @@ import platform.CoreLocation.kCLAuthorizationStatusAuthorizedWhenInUse
 
 @Suppress(names = ["EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING"])
 actual object PermissionUtil {
+    // CLLocation Manager
+    private val locationManager = CLLocationManager()
+
     actual fun isLocationPermissionGranted(): Boolean {
         // CLLocationManager의 상태 확인
-        val authStatus = CLLocationManager.authorizationStatus()
+        val authStatus = locationManager.authorizationStatus()
         // 상태 값에 따라 권한 부여 여부 판단
         val isGranted = when(authStatus) {
             kCLAuthorizationStatusAuthorizedWhenInUse,
@@ -20,7 +23,6 @@ actual object PermissionUtil {
     }
 
     actual fun requestLocationPermission() {
-        val locationManager = CLLocationManager()
         locationManager.requestWhenInUseAuthorization()
     }
 }
