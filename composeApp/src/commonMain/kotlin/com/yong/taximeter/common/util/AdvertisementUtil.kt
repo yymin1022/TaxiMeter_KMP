14eec5a58b6669b138d76ba7312687fe249c8934
yymin1022 <yymin1022@gmail.com>
2025-12-30 15:57:27 +0900

    Update AdvertisementUtil : Added comments

--- composeApp/src/commonMain/kotlin/com/yong/taximeter/common/util/AdvertisementUtil.kt
+++ composeApp/src/commonMain/kotlin/com/yong/taximeter/common/util/AdvertisementUtil.kt
@@ -5,7 +5,16 @@ import com.revenuecat.purchases.kmp.ktx.awaitCustomerInfo
 import com.yong.taximeter.common.def.PreferenceDef.KEY_AD_REMOVAL
 import com.yong.taximeter.ui.main.subscreen.store.StoreViewModel.Companion.SKU_ID_AD_REMOVE
 
+/**
+ * Advertisement Util
+ * - Logics about advertisement
+ */
 object AdvertisementUtil {
+    /**
+     * Checks if ad removal is purchased.
+     *
+     * @return Flag value about ad removal is purchased
+     */
     suspend fun isAdRemovalPurchased(): Boolean {
         try {
             val customerInfo = Purchases.sharedInstance.awaitCustomerInfo()
@@ -17,6 +26,11 @@ object AdvertisementUtil {
         }
     }
 
+    /**
+     * Update preference value about ad removal
+     *
+     * @param isEnabled Flag value about ad removal
+     */
     suspend fun updateAdRemovalPref(isEnabled: Boolean) {
         PreferenceUtil.putBoolean(KEY_AD_REMOVAL, isEnabled)
     }
